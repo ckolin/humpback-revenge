134245710bea7437e0b08c7518b7963640df6a1c
gorgovic <45965967+gorgovic@users.noreply.github.com>
2019-09-07 10:10:10 +0200

    Show boost particles

--- src/lib/whale.js
+++ src/lib/whale.js
@@ -2,9 +2,10 @@ class Whale {
     constructor() {
         this.thing = new Thing(new Sprite("whale", 4, 400), {x: 20, y: 60});
         this.lives = 3;
-        this.maxBoost = 1000;
+        this.maxBoost = 2000;
         this.boost = this.maxBoost;
         this.boostActivated = false;
+        this.bubbleEmitter = new Emitter([options.colors[11]], {x: 0, y: -.001}, 600, 1400, 1, 2, 0, 0.2);
     }
 
     hurt() {
@@ -23,21 +24,23 @@ class Whale {
 
         if (Vec.distance2(state.target, this.thing.position) > 10)
             this.thing.position = Vec.add(this.thing.position, Vec.scale(this.thing.forward, delta * 0.02));
+
+        this.bubbleEmitter.update(delta);
     }
 
     updateBoost(delta) {
         if (this.boost > 0 && state.input.boost) {
-            this.boost -= delta;
-            this.boost = Math.max(this.boost, 0);
+            this.boost = Math.max(this.boost - delta, 0);
+            this.bubbleEmitter.emit(this.thing.position, 1, 0);
             return true;
-        } else {
-            this.boost += delta * 0.5;
-            this.boost = Math.min(this.boost, this.maxBoost);
-            return false;
         }
+        if (!state.input.boost)
+            this.boost = Math.min(this.boost + delta * 0.3, this.maxBoost);
+        return false;
     }
 
     draw(ctx, time) {
+        this.bubbleEmitter.draw(ctx);
         this.thing.draw(ctx, time);
     }
 }
