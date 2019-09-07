8cea9630f49dccb60f21cea4d0634c75e4a7c314
gorgovic <45965967+gorgovic@users.noreply.github.com>
2019-09-07 10:10:49 +0200

    Add flag for sprite centering

--- src/lib/sprite.js
+++ src/lib/sprite.js
@@ -1,15 +1,16 @@
 class Sprite {
-    constructor(imageId, frames = 1, delay = 500, offset = 0) {
+    constructor(imageId, frames = 1, delay = 500, offset = 0, centered = true) {
         this.image = document.getElementById(imageId);
         this.frames = frames;
         this.frameWidth = Math.max(this.image.naturalWidth / frames, 1);
         this.delay = delay;
         this.offset = offset;
+        this.centered = centered;
     }
 
-    draw(ctx, time, scaleX) {
-        const frame = Math.floor(time / this.delay + this.offset) % this.frames;
-        ctx.translate(-this.frameWidth / 2, -this.image.naturalHeight / 2);
+    draw(ctx, time = 0, scaleX = 1) {
+        const frame = this.frames <= 1 ? 0 : Math.floor(time / this.delay + this.offset) % this.frames;
+        if (this.centered) ctx.translate(-this.frameWidth / 2, -this.image.naturalHeight / 2);
         ctx.scale(scaleX, 1);
         ctx.strokeStyle = options.colors[0];
         if (dbg()) ctx.strokeRect(0, 0, this.frameWidth * scaleX, this.image.naturalHeight);
