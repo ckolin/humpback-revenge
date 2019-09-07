51c2fa8d055dd9b75b178bec4887147f1b7a3a18
gorgovic <45965967+gorgovic@users.noreply.github.com>
2019-09-07 10:08:51 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -jar ~/.local/share/closure-compiler.jar--compilation_level ADVANCED_OPTIMIZATIONS --js src/lib/*.js src/script.js --js_output_file dist/dist.js
+java -jar ~/.local/share/closure-compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js src/lib/*.js src/script.js --js_output_file dist/dist.js
 advzip -4 -a dist.zip dist/*
