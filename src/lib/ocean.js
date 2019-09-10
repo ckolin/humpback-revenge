bbeef9ea1a3486f3b22239e8b2ccfe08932e9284
gorgovic <45965967+gorgovic@users.noreply.github.com>
2019-09-10 11:45:33 +0200

    Use delta time for ocean

--- src/lib/ocean.js
+++ src/lib/ocean.js
@@ -2,6 +2,7 @@ class Ocean {
     constructor(size = options.worldSize, level = 20) {
         this.size = size;
         this.level = level;
+        this.time = 0;
         this.sines = [
             {a: 1, b: 0.09, c: 0.001},
             {a: 1, b: 0.04, c: 0.0015},
@@ -16,7 +17,7 @@ class Ocean {
             render: (view, time) => {
                 view.callScaled((ctx) => {
                     for (let x = 0; x < this.size.x; x++) {
-                        const y = Math.floor(this.height(x, time));
+                        const y = Math.floor(this.height(x));
                         ctx.fillStyle = options.colors[12];
                         ctx.fillRect(x, y, 1, 2);
                         ctx.fillStyle = options.colors[11];
@@ -31,7 +32,7 @@ class Ocean {
                 view.callScaled((ctx) => {
                     ctx.fillStyle = options.colors[10];
                     for (let x = 0; x < this.size.x; x++) {
-                        const y = Math.floor(this.height(x, time));
+                        const y = Math.floor(this.height(x));
                         ctx.fillRect(x, y + 3, 1, this.size.y - y - 3);
                     }
                 });
@@ -45,7 +46,8 @@ class Ocean {
                 this.points[x].velocity += force;
     }
 
-    update() {
+    update(delta) {
+        this.time += delta;
         for (let x = 0; x < this.points.length; x++) {
             const point = this.points[x];
             const left = x > 0 ? this.points[x - 1].height - point.height : 0;
@@ -56,13 +58,13 @@ class Ocean {
         }
     }
 
-    sineSum(x, time) {
+    sineSum(x) {
         return this.sines
-            .map((s) => s.a * Math.sin(x * s.b + s.c * time))
+            .map((s) => s.a * Math.sin(x * s.b + s.c * this.time))
             .reduce((a, b) => a + b, 0);
     }
 
-    height(x, time) {
-        return this.level - this.points[x].height - this.sineSum(x + state.view.camera.x, time);
+    height(x) {
+        return this.level - this.points[x].height - this.sineSum(x + state.view.camera.x);
     }
 }
